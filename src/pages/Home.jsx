35ee88357fd3d929495541430289cc4e8dd75b7b
you@example.com <test\2gmail.com>
2025-03-08 22:27:54 +0100

    Worked on the first hero and added some images in the public

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
+import React from "react";
 import Header from "../components/Header";
+import "../styles/Home.css";
+
 const Home = () => {
     return(
-        <>
-        <Header/>
-
-        <div>Home</div>
-        </>
+        <div>
+        <Header />
+        <div className='hero-1'>
+         <div className='empty-div'>
+            
+         </div>
+         <div className='her0-1-write-up'>
+            <aside>
+            <h1>60% Discount</h1>
+            <h2>Winter Collection</h2>
+            <p>Best Cloth collection by 2020!</p>
+            </aside>
+            <button>Shop Now</button>
+         </div>
+      </div>
+        </div>
+      
+        
     )
     };
     export default Home;
