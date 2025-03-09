f338f6c266b78050b343f218e8cdf6a7982f77d1
you@example.com <test\2gmail.com>
2025-03-09 23:32:13 +0100

    signup complete

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "../styles/shoppingcart.css"
-import CartLandingPage from '../components/CartLandingPage'
+
 import Footer from '../components/Footer'
 
 const ShoppingCart = () => {
   return (
     <div className='mainShoppingCart'>
-      <CartLandingPage />
+    
       <Footer />
     </div>
   )
