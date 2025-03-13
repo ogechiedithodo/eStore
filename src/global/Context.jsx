3373187be94022691d6374910926dc826c8c8a90
Agu Chukwuebuka Ezekiel <aguchukwuebuka03@gmail.com>
2025-03-13 05:37:34 -0700

    add dropdown on my shopping cart and aslo work on the context, i aslo wrapped the contex on the main.jsx

--- src/global/Context.jsx
+++ src/global/Context.jsx
@@ -1,23 +1,34 @@
 import { useContext, useState, createContext } from "react";
-import PropTypes from "prop-types";
 
 const PostsContext = createContext();
 
 export const PostProvider = ({ children }) => {
+  const [dropmodalTwo, setDropModalTwo]= useState(false)
+   const [dropModal, setDropModal]=useState(false)
   const [allPost, setAllPost] = useState([]);
+  
+  const drop = ()=>{
+      setDropModal(!dropModal)
+      setDropModalTwo(false)
+  }
+  
+  const dropTwo = ()=>{
+    setDropModalTwo(!dropmodalTwo)
+    setDropModal(false)
+  }
 
   const value = {
     allPost,
     setAllPost,
-    getPost,
+    drop,
+    dropTwo,
+    dropmodalTwo,
+    dropModal,
   };
   return (
     <PostsContext.Provider value={value}>{children}</PostsContext.Provider>
   );
 };
 
-PostProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export const usePost = () => useContext(PostsContext);
